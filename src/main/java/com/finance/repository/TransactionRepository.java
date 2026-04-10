4ced5c4d7e60a2ec37d77c5bf13028f96490caa5
Jegadeshwaran b <jegadesh216@gmail.com>
2026-04-10 23:10:36 +0530

    Dashboard API added

--- src/main/java/com/finance/repository/TransactionRepository.java
+++ src/main/java/com/finance/repository/TransactionRepository.java
@@ -3,7 +3,14 @@ package com.finance.repository;
 import com.finance.entity.Transaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+	
 
+    @Query("SELECT SUM(t.amount) FROM Transaction t WHERE t.type = 'income'")
+    Double getTotalIncome();
+
+    @Query("SELECT SUM(t.amount) FROM Transaction t WHERE t.type = 'expense'")
+    Double getTotalExpense();
 }
